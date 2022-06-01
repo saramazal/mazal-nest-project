046f76266abf7e60db256809cd6340849ba3274c
Sara Mazal <32996161+saramazal@users.noreply.github.com>
2022-06-01 14:58:56 +0300

    adding SequelizeModule and Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import {AppController} from './app.controller'
 import {AppService} from './app.service'
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
     controllers : [AppController],
-    providers: [AppService]
+    providers: [AppService],
+    imports: [
+        SequelizeModule.forRoot({
+            dialect: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: 'root',
+            database: 'test',
+            models: [],
+          }),
+    ]
 })
 export class AppModule{}
