8de4a41e1befa0677672026aa71b1f0c16a5a84f
Sara Mazal <32996161+saramazal@users.noreply.github.com>
2022-06-02 13:41:08 +0300

    ading swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function start() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 5000
+
+const config = new DocumentBuilder()
+.setTitle('Mazal Nest Project')
+.setDescription('Mazal Nest')
+.setVersion('1.0.0')
+.addTag('Mazal Nest')
+.build()
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('/api/docs', app, document)
+
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
 start();
