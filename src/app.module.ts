1700c9789934ce0e835bea8ca99e123668aabbe5
Sara Mazal <32996161+saramazal@users.noreply.github.com>
2022-06-02 10:47:30 +0300

    adding nest generate module, controller and service users, nest config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,27 @@
 import { Module } from '@nestjs/common';
-import {AppController} from './app.controller'
-import {AppService} from './app.service'
+import{ConfigModule} from '@nestjs/config'
 import { SequelizeModule } from '@nestjs/sequelize';
+import { UsersModule } from './users/users.module';
 
 @Module({
-    controllers : [AppController],
-    providers: [AppService],
+    controllers : [],
+    providers: [],
     imports: [
+        ConfigModule.forRoot({
+            envFilePath: '.env'
+
+        }),
         SequelizeModule.forRoot({
-            dialect: 'mysql',
-            host: 'localhost',
-            port: 3306,
-            username: 'root',
-            password: 'root',
-            database: 'test',
+            dialect: 'postgres',
+            host: process.env.POSTGRES_HOST,
+            port: Number(process.env.POSTGRESS_PORT),
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRESS_PASSWORD,
+            database: process.env.POSTGRES_DB,
             models: [],
+            autoLoadModels: true
           }),
+        UsersModule,
     ]
 })
 export class AppModule{}
